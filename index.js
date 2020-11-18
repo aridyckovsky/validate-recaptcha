88cf194c1ec408d57ea345d7820c7a070b53317d
Ari Dyckovsky <aridyckovsky@gmail.com>
2020-11-18 14:10:37 -0700

    small input

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const AWS = require("aws-sdk");
 const https = require("https");
+
+exports.handler = async function(event, context) {
+	console.log("Environment variables: ", process.env);
+	console.log(event);
+	console.log(context);
+}
